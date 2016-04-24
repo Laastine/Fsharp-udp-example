ddb7d5c11c9c71cd14d873354bf70adfca323be8
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-04-24 11:09:10 +0300

    Typed JSON message sent to server

--- fsharpcodecamp/client.fs
+++ fsharpcodecamp/client.fs
@@ -3,21 +3,40 @@ namespace Client
 open System.Net
 open System.Net.Sockets
 open System.Text
+open System.Json
+open Fleece
+open Fleece.Operators
+
+type Connect = {
+  Type: string
+  Address: string
+}
+
+type Connect with
+  static member ToJSON (c: Connect) =
+    jobj [
+      "type" .= c.Type
+      "address" .= c.Address
+    ]
 
 type Client() =
   let serverAddress = new IPEndPoint(IPAddress.Any, 9999)
 
-  let serverAddr = "127.0.0.1"
-  let serverPort = 3000
+  let localAddr = "127.0.0.1"
+  let localPort = 3000
 
-  let msg = System.Text.Encoding.ASCII.GetBytes("hello world")
+  let jsonMsg = {
+                  Connect.Type = "connect"
+                  Address = localAddr
+                }
+  let msg = Encoding.ASCII.GetBytes((toJSON jsonMsg).ToString())
 
   let listen = async {
-    let ip = new IPEndPoint(IPAddress.Any, serverPort)
+    let ip = new IPEndPoint(IPAddress.Any, localPort)
     let clientSock = new UdpClient()
 
     let sender = new IPEndPoint(IPAddress.Any, 0)
-    printf "Listening port %d" serverPort
+    printf "Listening port %d" localPort
     let data = clientSock.Receive(ref sender)
     return data
   }
@@ -26,7 +45,7 @@ type Client() =
     let task = Async.StartAsTask (listen)
 
     let connection = new UdpClient()
-    let serverEndPoint = new IPEndPoint(IPAddress.Parse(serverAddr), serverPort)
+    let serverEndPoint = new IPEndPoint(IPAddress.Parse(localAddr), localPort)
     connection.Connect(serverEndPoint)
     connection.Send(msg, msg.Length) |> ignore
     printf "Client: %s" (Encoding.ASCII.GetString(msg))
