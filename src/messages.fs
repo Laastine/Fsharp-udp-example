4181444487be8e7e8fd173a9be563e3cbeb390d6
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-04-26 20:43:34 +0300

    Add FromJSON example function

--- src/messages.fs
+++ src/messages.fs
@@ -16,3 +16,23 @@ type Connect with
       "type" .= c.Type
       "address" .= c.Address
     ]
+
+type Response = {
+  Type: string
+  Msg: string
+}
+
+type Response with
+  static member FromJSON (_: Response) =
+    function
+    | JObject o ->
+      let ``type`` = o .@ "type"
+      let msg = o .@ "msg"
+      match ``type``, msg with
+      | Success ``type``, Success msg ->
+        Success {
+          Response.Type = ``type``
+          Response.Msg = msg
+        }
+      | x -> Failure (sprintf "Error parsing Response: %A" x)
+    | x -> Failure (sprintf "Expected Response, got %A" x)
